e2a8f4c90fc008bffa4695e4a351ba8d785336b7
NiangPro <NiangProgrammeur@gmail.com>
2024-10-02 15:17:42 +0000

    depot initial

---
+++ variables.php
@@ -0,0 +1,20 @@
+<?php
+
+    // les variables
+    $prenom = "Modou";
+    $age = 34;
+    $trouve = true;
+    $decimal = 78.9;
+    $nom = 'Diallo';
+
+    // les constantes 
+    define("PI", 3.14);
+
+
+    // les affichages 
+
+    echo $prenom;
+    echo "<br>Mon prenom: $prenom";
+    echo '<br>Nom:'.$nom;
+
+    echo '<br>PI:'.PI;
